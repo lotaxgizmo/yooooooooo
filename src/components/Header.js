113a45e6ab670e387cb31600cf296da45d1b7aac
Lotachi <lotaoko2@gmail.com>
2024-04-09 09:26:56 +0100

    dsfd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import dextools from "../assets/dextools.svg"
 import telegram from "../assets/telegram.svg"
@@ -8,9 +9,9 @@ function Header() {
         <div className='Mullers m-5'>
             <div className="two flex flex-col lg:flex-row  justify-between">
                 <div className="one border-black border-4 flex justify-between items-center align-middle ">
-                    <a href="#" target="_blank" rel="noopener noreferrer" className='text-2xl mx-3'>HOME</a>
-                    <a href="#" target="_blank" rel="noopener noreferrer" className='text-2xl mx-3'>ABOUT</a>
-                    <a href="#" target="_blank" rel="noopener noreferrer" className='text-2xl mx-3'>BUY</a>
+                    <a href="#" target="_blank" rel="noopener noreferrer" className='text-2xl lg:text-4xl mx-3'>HOME</a>
+                    <a href="#" target="_blank" rel="noopener noreferrer" className='text-2xl lg:text-4xl mx-3'>ABOUT</a>
+                    <a href="#" target="_blank" rel="noopener noreferrer" className='text-2xl lg:text-4xl mx-3'>BUY</a>
                 </div>
                 <div className="one border-black border-4 flex justify-between items-center align-middle ">
                     <a href="#" target="_blank" rel="noopener noreferrer" className=' mx-3'>
